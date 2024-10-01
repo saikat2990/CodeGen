d04564f5da2e9a0df64ff2e7eb518d1052554ae0
saikat sen <saikat2990@gmail.com>
2024-10-01 19:42:09 +0600

    feat: add category endpoints

---
+++ src/Services/Product/Product.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Product.Application.Features.Category.Commands;
+using Product.Application.Features.Category.Queries;
+
+namespace Product.Api.Controllers;
+
+public class CategoriesController : BaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+    {
+        var categoryId = await Sender.Send(command);
+        return CreatedAtAction(nameof(GetCategory), new { id = categoryId }, null);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCategories()
+    {
+        var categories = await Sender.Send(new GetAllCategoryQuery());
+        return Ok(categories);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCategory(int id)
+    {
+        var category = await Sender.Send(new GetCategoryByIdQuery { Id = id });
+        return Ok(category);
+    }
+}
