ff75d915cf6e022c34bed20c741ee0e72f069093
saikat sen <saikat2990@gmail.com>
2024-10-09 14:49:35 +0600

    mapping profile added

---
+++ src/Services/Product/Product.Application/MappingProfiles/AppComponentMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Product.Application.Features.AppComponent.Commands;
+using Product.Application.Features.AppComponent.Queries;
+using Product.Domain.Entities;
+
+namespace Product.Application.MappingProfiles;
+public class AppComponentMappingProfile : Profile
+{
+    public AppComponentMappingProfile()
+    {
+        CreateMap<AppComponent, AppComponentResponse>()
+            .ForMember(d => d.ApplicationMenus, opt => opt.MapFrom(s => s.ApplicationMenus));
+
+        CreateMap<AddOrUpdateAppComponentCommand, AppComponent>();
+    }
+}
---
+++ src/Services/Product/Product.Application/MappingProfiles/ApplicationMenuMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Product.Application.Features.ApplicationMenu.Commands;
+using Product.Application.Features.ApplicationMenu.Queries;
+using Product.Domain.Entities;
+
+namespace Product.Application.MappingProfiles;
+public class ApplicationMenuMappingProfile : Profile
+{
+    public ApplicationMenuMappingProfile()
+    {
+        CreateMap<ApplicationMenu, ApplicationMenuResponse>();
+        CreateMap<AddOrUpdateApplicationMenuCommand, ApplicationMenu>();
+    }
+}
