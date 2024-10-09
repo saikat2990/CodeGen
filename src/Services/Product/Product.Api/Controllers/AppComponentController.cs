b83ccdd08274e7e2d87443e45824c60e77f1946c
saikat sen <saikat2990@gmail.com>
2024-10-09 18:51:48 +0600

    Controller added

---
+++ src/Services/Product/Product.Api/Controllers/AppComponentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Product.Application.Features.AppComponent.Commands;
+using Product.Application.Features.AppComponent.Queries;
+
+namespace Product.Api.Controllers;
+
+public class AppComponentController : BaseController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetList()
+    {
+        var appComponents = await Sender.Send(new GetAllAppComponentQuery());
+        return Ok(appComponents);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var appComponent = await Sender.Send(new GetAppComponentByIdQuery { Id = id });
+        return Ok(appComponent);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddOrUpdate(AddOrUpdateAppComponentCommand command)
+    {
+        var response = await Sender.Send(command);
+        return response.IsSuccess ? Ok(response) : BadRequest(response);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(DeleteAppComponentCommand command)
+    {
+        var response = await Sender.Send(command);
+        return response.IsSuccess ? NoContent() : BadRequest(response);
+    }
+}
---
+++ src/Services/Product/Product.Api/Controllers/ApplicationMenuController.cs
@@ -0,0 +1,37 @@
+﻿using ApplicationMenu.Application.Features.ApplicationMenu.Commands;
+using ApplicationMenu.Application.Features.ApplicationMenu.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Product.Application.Features.ApplicationMenu.Commands;
+
+namespace Product.Api.Controllers;
+public class ApplicationMenuController : BaseController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetList()
+    {
+        var applicationMenus = await Sender.Send(new GetAllApplicationMenuQuery());
+        return Ok(applicationMenus);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var applicationMenu = await Sender.Send(new GetApplicationMenuByIdQuery { Id = id });
+        return Ok(applicationMenu);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddOrUpdate(AddOrUpdateApplicationMenuCommand command)
+    {
+        var response = await Sender.Send(command);
+        return response.IsSuccess ? Ok(response) : BadRequest(response);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(DeleteApplicationMenuCommand command)
+    {
+        var response = await Sender.Send(command);
+        return response.IsSuccess ? NoContent() : BadRequest(response);
+    }
+}
