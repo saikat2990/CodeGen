78f9c9ac95f10197e6523e76c5cf5d9c720120be
saikat sen <saikat2990@gmail.com>
2024-10-09 18:01:48 +0600

    delete command added

---
+++ src/Services/Product/Product.Application/Features/AppComponent/Commands/DeleteAppComponentCommand.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Product.Application.Common;
+using Product.Application.Interfaces;
+using Shared.Contracts;
+using Shared.Infrastructures.RequestHandlers;
+
+namespace Product.Application.Features.AppComponent.Commands;
+public class DeleteAppComponentCommand : IRequest<ApiResponse<bool>>
+{
+    public List<Guid> IdList { get; set; }
+}
+
+public class DeleteAppComponentHandler : BaseRequestHandler<DeleteAppComponentCommand, ApiResponse<bool>, Domain.Entities.AppComponent, Guid>
+{
+    public DeleteAppComponentHandler(IProductUnitOfWork uow, IMapper mapper) : base(uow, mapper) { }
+
+    public override async Task<ApiResponse<bool>> HandleRequest(DeleteAppComponentCommand request, CancellationToken ctn)
+    {
+        var recordsDeleted = await _repository.BulkDeleteAsync(request.IdList, ctn);
+        return recordsDeleted > 0 ? ApiResponse<bool>.Success(true) : ApiResponse<bool>.Failure(Constants.DeleteFailedMsg);
+    }
+}
---
+++ src/Services/Product/Product.Application/Features/ApplicationMenu/Commands/DeleteApplicationMenuCommand.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Shared.Contracts;
+using Shared.Infrastructures.RequestHandlers;
+using Product.Application.Interfaces;
+using Product.Application.Common;
+
+namespace ApplicationMenu.Application.Features.ApplicationMenu.Commands;
+public class DeleteApplicationMenuCommand : IRequest<ApiResponse<bool>>
+{
+    public List<Guid> IdList { get; set; }
+}
+
+public class DeleteApplicationMenuCommandHandler : BaseRequestHandler<DeleteApplicationMenuCommand, ApiResponse<bool>, Product.Domain.Entities.ApplicationMenu, Guid>
+{
+    public DeleteApplicationMenuCommandHandler(IProductUnitOfWork uow, IMapper mapper) : base(uow, mapper)
+    {
+    }
+
+    public override async Task<ApiResponse<bool>> HandleRequest(DeleteApplicationMenuCommand request, CancellationToken ctn)
+    {
+        var recordsDeleted = await _repository.BulkDeleteAsync(request.IdList, ctn);
+
+        return recordsDeleted > 0 ? ApiResponse<bool>.Success(true) : ApiResponse<bool>.Failure(Constants.DeleteFailedMsg);
+    }
+}
