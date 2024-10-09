be4bab28b9d57b8ef5ed564355d422a639480ddc
saikat sen <saikat2990@gmail.com>
2024-10-09 15:30:08 +0600

    GetAllAppComponent Added

---
+++ src/Services/Product/Product.Application/Features/AppComponent/Queries/GetAllAppComponentQuery.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Product.Application.Interfaces;
+using Shared.Contracts;
+using Shared.Infrastructures.RequestHandlers;
+
+namespace Product.Application.Features.AppComponent.Queries;
+public class GetAllAppComponentQuery : IRequest<ApiResponse<IEnumerable<AppComponentResponse>>>
+{
+}
+
+public class GetAllAppComponentQueryHandler : BaseRequestHandler<GetAllAppComponentQuery, ApiResponse<IEnumerable<AppComponentResponse>>, Domain.Entities.AppComponent, Guid>
+{
+    public GetAllAppComponentQueryHandler(IProductUnitOfWork uow, IMapper mapper) : base(uow, mapper)
+    {
+    }
+
+    public override async Task<ApiResponse<IEnumerable<AppComponentResponse>>> HandleRequest(GetAllAppComponentQuery request, CancellationToken ctn)
+    {
+        var categories = await _repository
+            .GetAll()
+            .Include(c => c.ApplicationMenus)
+            .ProjectTo<AppComponentResponse>(_mapper.ConfigurationProvider)
+            .ToListAsync(ctn);
+
+        return ApiResponse<IEnumerable<AppComponentResponse>>.Success(categories);
+    }
+}
--- src/Services/Product/Product.Application/Features/Category/Queries/CategoryResponse.cs
+++ src/Services/Product/Product.Application/Features/Category/Queries/CategoryResponse.cs
@@ -7,6 +7,5 @@ public class CategoryResponse
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-
     public IEnumerable<ProductResponse> Products { get; set; } = [];
 }
