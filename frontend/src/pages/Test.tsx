8f017bc8229a949f81143db9b65af75aa4376142
Maruf Hossain <mhtamun@gmail.com>
2024-10-10 17:20:36 +0600

    IMplement json sample for dynamic list view engine

--- frontend/src/pages/Test.tsx
+++ frontend/src/pages/Test.tsx
@@ -1,40 +1,55 @@
 import DataTable from "@/components/general/DataTable";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
+// A method that will take JSON input and create column definitions
 export const createColumnsFromJson = (columnDefs: any[]): ColumnDef<any>[] => {
   return columnDefs
     .map((colDef) => {
       switch (colDef.type) {
-        case "select":
-          return {
-            id: "select",
-            header: ({ table }) => (
-              <Checkbox
-                checked={
-                  table.getIsAllPageRowsSelected() ||
-                  (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) =>
-                  table.toggleAllPageRowsSelected(!!value)
-                }
-                aria-label="Select all"
-              />
-            ),
-            cell: ({ row }) => (
-              <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-              />
-            ),
-            enableSorting: false,
-            enableHiding: false,
-          };
+        // case "select":
+        //   return {
+        //     id: "select",
+        //     header: ({ table }) => (
+        //       <Checkbox
+        //         checked={
+        //           table.getIsAllPageRowsSelected() ||
+        //           (table.getIsSomePageRowsSelected() && "indeterminate")
+        //         }
+        //         onCheckedChange={(value) =>
+        //           table.toggleAllPageRowsSelected(!!value)
+        //         }
+        //         aria-label="Select all"
+        //       />
+        //     ),
+        //     cell: ({ row }) => (
+        //       <Checkbox
+        //         checked={row.getIsSelected()}
+        //         onCheckedChange={(value) => row.toggleSelected(!!value)}
+        //         aria-label="Select row"
+        //       />
+        //     ),
+        //     enableSorting: colDef.enableSorting ?? false,
+        //     enableHiding: colDef.enableHiding ?? false,
+        //   };
 
         case "text":
           return {
             accessorKey: colDef.accessorKey,
             header: colDef.header,
             cell: ({ row }) => <div>{row.getValue(colDef.accessorKey)}</div>,
+            enableSorting: colDef.enableSorting ?? true,
+            enableHiding: colDef.enableHiding ?? true,
           };
 
         case "email":
@@ -58,6 +73,8 @@ export const createColumnsFromJson = (columnDefs: any[]): ColumnDef<any>[] => {
                 {row.getValue(colDef.accessorKey)}
               </div>
             ),
+            enableSorting: colDef.enableSorting ?? true,
+            enableHiding: colDef.enableHiding ?? true,
           };
 
         case "amount":
@@ -73,12 +90,14 @@ export const createColumnsFromJson = (columnDefs: any[]): ColumnDef<any>[] => {
 
               return <div className="text-right font-medium">{formatted}</div>;
             },
+            enableSorting: colDef.enableSorting ?? true,
+            enableHiding: colDef.enableHiding ?? true,
           };
 
         case "actions":
           return {
             id: "actions",
-            enableHiding: false,
+            header: () => <div className="text-left">Actions</div>,
             cell: ({ row }) => {
               const payment = row.original;
               return (
@@ -103,6 +122,8 @@ export const createColumnsFromJson = (columnDefs: any[]): ColumnDef<any>[] => {
                 </DropdownMenu>
               );
             },
+            enableSorting: colDef.enableSorting ?? false,
+            enableHiding: colDef.enableHiding ?? false,
           };
 
         default:
@@ -113,19 +134,99 @@ export const createColumnsFromJson = (columnDefs: any[]): ColumnDef<any>[] => {
 };
 
 const Test: React.FC = () => {
+  const json = {
+    title: "Test Title",
+    subtitle: "Test Subtitle",
+    columns: [
+      // {
+      //   type: "select",
+      //   enableSorting: false,
+      //   enableHiding: false,
+      // },
+      {
+        type: "text",
+        accessorKey: "id",
+        header: "Code",
+        enableSorting: true,
+        enableHiding: true,
+      },
+      {
+        type: "text",
+        accessorKey: "status",
+        header: "Status",
+        enableSorting: true,
+        enableHiding: true,
+      },
+      {
+        type: "email",
+        accessorKey: "email",
+        header: "Email",
+        enableSorting: true,
+        enableHiding: true,
+      },
+      {
+        type: "amount",
+        accessorKey: "amount",
+        header: "Amount",
+        enableSorting: true,
+        enableHiding: true,
+      },
+      {
+        type: "actions",
+        enableSorting: false,
+        enableHiding: false,
+      },
+    ],
+    data: [
+      {
+        id: "m5gr84i9",
+        amount: 316,
+        status: "success",
+        email: "ken99@yahoo.com",
+      },
+      {
+        id: "3u1reuv4",
+        amount: 242,
+        status: "success",
+        email: "Abe45@gmail.com",
+      },
+      {
+        id: "derv1ws0",
+        amount: 837,
+        status: "processing",
+        email: "Monserrat44@gmail.com",
+      },
+      {
+        id: "5kma53ae",
+        amount: 874,
+        status: "success",
+        email: "Silas22@gmail.com",
+      },
+      {
+        id: "bhqecj4p",
+        amount: 721,
+        status: "failed",
+        email: "carmella@hotmail.com",
+      },
+    ],
+    searchPlaceholder: "Search email",
+    searchableColumn: "email",
+  };
+
   return (
     <div className="sm:pl-64">
       <div className="mb-2 flex items-center justify-between space-y-2">
         <div>
-          <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
-          <p className="text-muted-foreground">
-            Here&apos;s a list of your tasks for this month!
-          </p>
+          <h2 className="text-2xl font-bold tracking-tight">{json.title}</h2>
+          <p className="text-muted-foreground">{json.subtitle}</p>
         </div>
       </div>
-      {/* <div className="flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0"> */}
-      <DataTable />
-      {/* </div> */}
+      <DataTable
+        columns={createColumnsFromJson(json.columns)}
+        data={json.data}
+        searchPlaceholder={json.searchPlaceholder}
+        searchableColumn={json.searchableColumn}
+      />
     </div>
   );
 };
