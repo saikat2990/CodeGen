0c2a5fa7b4984d1aa76a49fb999701908ce392e9
saikat sen <saikat2990@gmail.com>
2024-10-14 17:00:07 +0600

    refactoring

--- backend/src/Services/Seed/Viva.Seed.Infrastructure/Persistence/VivaSeedDbContext.cs
+++ backend/src/Services/Seed/Viva.Seed.Infrastructure/Persistence/VivaSeedDbContext.cs
@@ -12,7 +12,7 @@ public class VivaSeedDbContext : IdentityDbContext<ApplicationUser, Role, int>
     {
     }
 
-    public DbSet<Domain.Entities.Product> Products { get; set; }
+    public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Right> Rights { get; set; }
     public DbSet<AppComponent> AppComponents { get; set; }
